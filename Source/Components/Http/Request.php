ad7060e43cd6950b05a8c7cab8d77a9a67e549c3
Juraj Blahunka <sul1nko@gmail.com>
2010-04-01 22:28:58 +0200

    Added Request class

---
+++ Source/Components/Http/Request.php
@@ -0,0 +1,60 @@
+<?php
+
+class Request
+{
+	private
+		$parameters,
+		$cookies,
+		$server;
+
+	public function __construct($get = null, $post = null, $cookies = null, $server = null)
+	{
+		if ($get === null)     $get     = $_GET;
+		if ($post === null)	   $post    = $_POST;
+		if ($cookies === null) $cookies = $_COOKIE;
+		if ($server === null)  $server  = $_SERVER;
+
+		$this->parameters = array_merge($get, $post);
+		$this->cookies    = $cookies;
+		$this->server     = $server;
+	}
+	
+	public function hasParameter($parameterKey)
+	{
+		return isset($this->parameters[$parameterKey]);
+	}
+
+	public function getParameter($parameterKey, $defaultValue = null)
+	{
+		return $this->hasParameter($parameterKey)
+			? $this->parameters[$parameterKey]
+			: $defaultValue;
+	}
+
+	public function hasCookie($cookieKey)
+	{
+		return isset($this->cookies[$cookieKey]);
+	}
+
+	public function getCookie($cookieKey, $defaultValue = null)
+	{
+		return $this->hasCookie($cookieKey)
+			? $this->cookies[$cookieKey]
+			: $defaultValue;
+	}
+
+	public function hasServer($serverKey)
+	{
+		return isset($this->server[$serverKey]);
+	}
+
+	public function getServer($serverKey, $defaultValue = null)
+	{
+		return $this->hasServer($serverKey)
+			? $serverKey
+			: $defaultValue;
+	}
+}
+
+// usage
+$r = new Request();
