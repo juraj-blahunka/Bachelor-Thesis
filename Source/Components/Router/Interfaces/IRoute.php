3ca43a3126f8094ba68cd5ddb0f3ac03310c0d00
Juraj Blahunka <sul1nko@gmail.com>
2010-04-06 19:45:09 +0200

    Updated Route Interface

--- Source/Components/Router/Interfaces/IRoute.php
+++ Source/Components/Router/Interfaces/IRoute.php
@@ -2,10 +2,12 @@
 
 interface IRoute
 {
+	function setPackage($package);
 	function setController($controller);
 	function setAction($action);
 	function setParameters(array $params);
 
+	function getPackage();
 	function getController();
 	function getAction();
 	function getParameters();
