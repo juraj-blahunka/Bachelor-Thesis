dc78885c0dead70faa0e03063fff7dbf03424180
Juraj Blahunka <sul1nko@gmail.com>
2010-04-11 20:10:47 +0200

    ReflectionCache facade initializes class reflections from methods and looks into class reflections for searched methods, if class ref. exist

--- Source/Components/Cache/Reflection/ReflectionCache.php
+++ Source/Components/Cache/Reflection/ReflectionCache.php
@@ -34,17 +34,34 @@ class ReflectionCache implements IReflectionCache
 
 	public function storeMethod(ReflectionMethod $reflection)
 	{
+		$classReflection = $reflection->getDeclaringClass();
+		if (! $this->classes->hasClass($classReflection->getName()))
+			$this->classes->storeClass($classReflection);
+
 		return $this->methods->storeMethod($reflection);
 	}
 
 	public function retrieveMethod($class, $method)
 	{
-		return $this->methods->retrieveMethod($class, $method);
+		if ($this->hasMethod($class, $method))
+			return $this->methods->retrieveMethod($class, $method);
 	}
 
 	public function hasMethod($class, $method)
 	{
-		return $this->methods->hasMethod($class, $method);
+		if ($this->methods->hasMethod($class, $method))
+			return true;
+		if ($this->classes->hasClass($class))
+		{
+			$classReflection = $this->classes->retrieveClass($class);
+			if ($classReflection->hasMethod($method))
+			{
+				$methodReflection = $classReflection->getMethod($method);
+				$this->methods->storeMethod($methodReflection);
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public function deleteMethod($class, $method)
