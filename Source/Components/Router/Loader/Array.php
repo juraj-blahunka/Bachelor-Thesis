1bd33d05a16c9daf5138977c7195bc3baa5e59d5
Juraj Blahunka <sul1nko@gmail.com>
2010-04-16 22:38:19 +0200

    Updated RoutingRuleLoader

--- Source/Components/Router/Loader/Array.php
+++ Source/Components/Router/Loader/Array.php
@@ -2,6 +2,13 @@
 
 class RoutingRuleArrayLoader
 {
+	protected $defaultPackage;
+
+	public function __construct($defaultPackage = null)
+	{
+		$this->defaultPackage = $defaultPackage;
+	}
+
 	public function load(array $array)
 	{
 		$result = array();
@@ -10,10 +17,22 @@ class RoutingRuleArrayLoader
 			$result[] = new RoutingRule(
 				$name,
 				$rule['pattern'],
-				isset($rule['params']) ? $rule['params'] : array(),
+				$this->parseParams(isset($rule['params']) ? $rule['params'] : array()),
 				isset($rule['rules'])  ? $rule['rules'] : array()
 			);
 		}
 		return $result;
+	}
+
+	private function parseParams(array $params)
+	{
+		if (! isset($params['package']))
+		{
+			if ($this->defaultPackage !== null)
+				$params['package'] = $this->defaultPackage;
+			else
+				throw new RuntimeException(sprintf("No package set four rule with parameters: %s", var_export($params, true)));
+		}
+		return $params;
 	}
 }
