f57f2ce0e36dcef74f14389e4daa0d44df106d1f
Juraj Blahunka <sul1nko@gmail.com>
2010-04-16 22:11:29 +0200

    Router array loader

--- Source/Components/Router/Loader/Array.php
+++ Source/Components/Router/Loader/Array.php
@@ -2,8 +2,18 @@
 
 class RoutingRuleArrayLoader
 {
-	public function load($array)
+	public function load(array $array)
 	{
-
+		$result = array();
+		foreach ($array as $name => $rule)
+		{
+			$result[] = new RoutingRule(
+				$name,
+				$rule['pattern'],
+				isset($rule['params']) ? $rule['params'] : array(),
+				isset($rule['rules'])  ? $rule['rules'] : array()
+			);
+		}
+		return $result;
 	}
 }
