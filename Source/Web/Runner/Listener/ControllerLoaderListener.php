4f60660d72f3122c96489656265cf5b7f6efcf5f
Juraj Blahunka <sul1nko@gmail.com>
2010-04-16 14:16:20 +0200

    ControllerLoaderListener - handles 'controller.load' event

---
+++ Source/Web/Runner/Listener/ControllerLoaderListener.php
@@ -0,0 +1,23 @@
+<?php
+
+class ControllerLoaderListener
+{
+	private $loader;
+
+	public function __construct(ControllerLoader $loader)
+	{
+		$this->loader = $loader;
+	}
+	public function handle(IEvent $event)
+	{
+		$route      = $event->getParameter('route');
+		$controller = $this->loader->loadController($route->getController());
+		if ($controller)
+		{
+			$event->setValue($controller);
+			return true;
+		}
+		else
+			return false;
+	}
+}
