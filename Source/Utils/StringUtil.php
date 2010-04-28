a236cb0f721b6a148e26f4d4068c7a6db00d88f2
Juraj Blahunka <sul1nko@gmail.com>
2010-04-28 14:17:39 +0200

    Adde StringUtil::slugify

--- Source/Utils/StringUtil.php
+++ Source/Utils/StringUtil.php
@@ -4,7 +4,7 @@ class StringUtil
 {
 	/**
 	 * Underscores and hyphens represent a word separator, which is
-	 * removed and next character is capitalized
+	 * removed and next character is capitalized.
 	 *
 	 * @param string $string
 	 * @return string
@@ -15,4 +15,22 @@ class StringUtil
 		$upper = ucwords($words);
         return str_replace(' ','', $upper);
     }
+
+	/**
+	 * Converts a string to slugified representation. Removes non alpha numeric
+	 * characters, substitutes spaces with hyphens.
+	 * Eg: "Hello World!" becomes "hello-world"
+	 *
+	 * @param string $string
+	 * @return string
+	 */
+	static public function slugify($string)
+	{
+		$slug = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+		$slug = strtolower($slug);
+		$slug = preg_replace('/[^a-z0-9- ]/', '', $slug ); // remove all non-alphanumeric characters except for spaces and hyphens
+		$slug = str_replace(' ', '-', $slug);              // substitute spaces with hyphens
+		$slug = preg_replace('/(\-)+/', '-', $slug);       // remove continuous hyphens
+		return $slug;
+	}
 }
